b9bcc119025c2380533b7a92801e740c67fe8119
serii burduja <seriiburduja@gmail.com>
2025-05-13 10:21:19 +0300

    feat: routes changed

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return response()->json(['message' => 'Welcome to the API']);
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
