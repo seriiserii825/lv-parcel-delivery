7d508599fecfda3a1e4ab6c69d8886636841dadb
serii burduja <seriiburduja@gmail.com>
2025-05-14 22:31:11 +0300

    feat: client migration and seeder

---
+++ database/seeders/ClientSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $clients = [
+            ['name' => 'Blueline'],
+            ['name' => 'Alutofianco'],
+            ['name' => 'Rewind']
+        ];
+
+        foreach ($clients as $client) {
+            \App\Models\Client::create($client);
+        }
+    }
+}
